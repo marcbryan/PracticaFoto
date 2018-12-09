d66fcca3c06ba0dc282a9bd405139fb057db204e
marcbryan <marcboakyeflores98@gmail.com>
2018-12-09 18:16:53 +0100

    Fix guardar imagen v3

--- app/src/main/java/com/camara/et/camaraex/MainActivity.java
+++ app/src/main/java/com/camara/et/camaraex/MainActivity.java
@@ -2,14 +2,18 @@ package com.camara.et.camaraex;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,9 +22,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
-    private Intent takePictureIntent;
     private File photoFile = null;
 
     @Override
@@ -31,32 +33,44 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Hacer foto con camara
-                takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                System.out.println("onClick");
-                //if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    try {
-                        photoFile = createImageFile();
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-
-
-                    }catch (IOException e){
-                        e.printStackTrace();
-                    }
-
-                //}
+                openCamera();
             }
         });
     }
 
 
+    private void openCamera() {
+        //Hacer foto con camara
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.camara.et.camaraex", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            setPic();
+
+            Toast toast = Toast.makeText(getApplicationContext(), "Imagen guardada correctamente!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = new File("/storage/sdcard/Android/data/com.camara.et.camaraex/files/Pictures");//ruta donde se guardara la foto
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);//ruta donde se guardara la foto
         File image = File.createTempFile(
                 imageFileName,
                 ".jpg",
@@ -65,29 +79,30 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+    private void setPic() {
+        ImageView foto = findViewById(R.id.foto);
+        
+        // Get the dimensions of the View
+        int targetW = foto.getWidth();
+        int targetH = foto.getHeight();
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoFile.getPath(), bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
 
-            //Pone la foto que he realizado en el ImageView
-            ImageView foto = findViewById(R.id.foto);
-            foto.setImageBitmap(imageBitmap);
-            //Guarda la imagen
-            /*
-            try {
-                photoFile = createImageFile();
-            } catch (IOException e){
-                e.printStackTrace();
-            }*/
-            Uri uriSavedImage = Uri.fromFile(photoFile);
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage("Imagen guardada correctamente!");
-            builder.setTitle("Información");
-            builder.create();
-            builder.show();
-        }
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        //bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getPath(), bmOptions);
+        //Pone la foto que he realizado en el ImageView
+        foto.setImageBitmap(bitmap);
     }
 }
